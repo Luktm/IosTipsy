03a7be2f10d36ad66aa0b2743dfc70d73fb1c33b
cspeed <cspeed@cspeeds-MacBook-Pro-2.local>
2020-05-04 22:46:29 +0800

    first commit

---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,25 @@
+
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingLabel: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func recalcuatePressed(_ sender: UIButton) {
+        
+    }
+    
+   
+
+}
